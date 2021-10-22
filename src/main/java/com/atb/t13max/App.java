736f631603da7f55def0b7a4b11bce509067ffed
atb <1>
2021-10-22 16:29:01 +0800

    呆呆的宝贝库创建啦

---
+++ src/main/java/com/atb/t13max/App.java
@@ -0,0 +1,11 @@
+package com.atb.t13max;
+
+/**
+ * @Author 呆呆
+ * @Datetime 2021/10/22 16:28
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
