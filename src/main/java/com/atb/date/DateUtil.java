db3ec28275b89f5837d38066e74eabbeb1a25110
atb <1>
2021-11-15 15:39:58 +0800

    时间戳转日期时间

---
+++ src/main/java/com/atb/date/DateUtil.java
@@ -0,0 +1,16 @@
+package com.atb.date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author 呆呆
+ * @Datetime 2021/11/15 14:19
+ */
+public class DateUtil {
+    public static void main(String[] args) {
+        Date date = new Date(1630911720459L);
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH-mm");
+        System.out.println(sdf.format(date));
+    }
+}
