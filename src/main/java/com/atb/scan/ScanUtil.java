c17c5ec7f61241808e7c722f7c9720847969ce1f
atb <1>
2022-04-29 16:18:58 +0800

    好耶

---
+++ src/main/java/com/atb/scan/ScanUtil.java
@@ -0,0 +1,21 @@
+package com.atb.scan;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @Author 呆呆
+ * @Datetime 2022/4/26 19:40
+ */
+public class ScanUtil {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Set<Class<?>> scan(String pack) {
+        Set<Class<?>> result = new LinkedHashSet<>();
+
+        return result;
+    }
+}
